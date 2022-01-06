415f95606ab6c7de141aaf4b6f0270a2039f07de
jhoisz <jvrda@ufpi.edu.br>
2022-01-06 13:55:44 -0300

    Added a list of movies.

--- lib/src/home/homepage.dart
+++ lib/src/home/homepage.dart
@@ -1,93 +1,125 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart'; 
+import 'package:google_fonts/google_fonts.dart';
+import 'package:moviesapp/src/home/models/movie.dart';
 import 'widgets/movie_card.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Movie> listMovies = [];
+
   var movies = [
-    {'name: ': 'Interestelar (2014)', 'image':'https://lojasaraiva.vteximg.com.br/arquivos/ids/12105538/1002443202.jpg?v=637142234580400000'},
-    {'name': 'Ponyo (2010)', 'image':'https://upload.wikimedia.org/wikipedia/pt/e/e2/Ponyo_p%C3%B4ster.png'},
-    {'name': 'Mr. Nobody (2009)', 'image':'https://m.media-amazon.com/images/I/91WY2zIvzzL._AC_SY550_.jpg'},
-    {'name': 'O Hobbit: A Desolação de Smaug (2013) ', 'image': 'https://br.web.img3.acsta.net/pictures/210/571/21057125_20131112201221324.jpg'}
+    {
+      'name: ': 'AAAAAA',
+      'image':
+          'https://lojasaraiva.vteximg.com.br/arquivos/ids/12105538/1002443202.jpg?v=637142234580400000'
+    },
+    {
+      'name': 'Ponyo (2010)',
+      'image':
+          'https://upload.wikimedia.org/wikipedia/pt/e/e2/Ponyo_p%C3%B4ster.png'
+    },
+    {
+      'name': 'Mr. Nobody (2009)',
+      'image': 'https://m.media-amazon.com/images/I/91WY2zIvzzL._AC_SY550_.jpg'
+    },
+    {
+      'name': 'O Hobbit: A desolação de Smaug (2013)',
+      'image':
+          'https://br.web.img3.acsta.net/pictures/210/571/21057125_20131112201221324.jpg'
+    },
+    {
+      'name': 'Mr. Nobody (2009)',
+      'image': 'https://m.media-amazon.com/images/I/91WY2zIvzzL._AC_SY550_.jpg'
+    },
+    {
+      'name': 'O Hobbit: A desolação de Smaug (2013) ',
+      'image':
+          'https://br.web.img3.acsta.net/pictures/210/571/21057125_20131112201221324.jpg'
+    },
+    {
+      'name': 'Mr. Nobody (2009)',
+      'image': 'https://m.media-amazon.com/images/I/91WY2zIvzzL._AC_SY550_.jpg'
+    },
+    {
+      'name': 'O Hobbit: A desolação de Smaug (2013)',
+      'image':
+          'https://br.web.img3.acsta.net/pictures/210/571/21057125_20131112201221324.jpg'
+    }
   ];
+
+  @override
+  void initState() {
+    for (var element in movies) {
+      listMovies.add(Movie.fromJson(element));
+    }
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
-       color: Colors.white,
-       child: Column(
+      color: Colors.white,
+      child: Column(
+        children: [
+          //SEARCH BAR
+          Container(
+              margin: const EdgeInsets.fromLTRB(0, 50.0, 0, 20.0),
+              width: MediaQuery.of(context).size.width / 1.2,
+              height: 45.0,
+              decoration: BoxDecoration(
+                  color: const Color(0xFFF4F4F4),
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade300,
+                        blurRadius: 8,
+                        offset: const Offset(0, 6))
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                    onChanged: (text) {
+                      print('Campo de texto: $text');
+                    },
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        labelText: 'Search',
+                        labelStyle:
+                            TextStyle(color: Color(0xFF7E7E7E), fontSize: 14),
+                        // border: disabledBorder,
+                        suffixIcon: Icon(Icons.search))),
+              )),
 
-         children: [
-
-           //SEARCH BAR
-           Container(
-             margin: const EdgeInsets.fromLTRB(0, 50.0, 0, 20.0),
-             width: MediaQuery.of(context).size.width/1.2,
-             height: 45.0,
-             decoration: BoxDecoration(
-               color: const Color(0xFFF4F4F4),
-               borderRadius: BorderRadius.circular(15),
-               boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade300,
-                    blurRadius: 8,
-                    offset: const Offset(0, 6)
-                 )
-               ]
-             ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextFormField(
-                onChanged: (text){
-                  print('Campo de texto: $text');
-                },
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  labelText: 'Search',
-                  labelStyle: TextStyle(
-                    color: Color(0xFF7E7E7E),
-                    fontSize: 14
-                  ),
-                  // border: disabledBorder,
-                  suffixIcon: Icon(
-                    Icons.search
-                  )
-                )
-              ),
-            )
-           ),
-
-           //MOVIES TEXT
-           Container(
-             alignment: Alignment.topLeft,
-             padding: const EdgeInsets.fromLTRB(35, 0, 0, 10),
-             child: Text(
-                'Movies',
-                textAlign: TextAlign.left,
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 32
-                )
-             )
-           ),
-
-          // GridView.count(
-          //   crossAxisCount: 2,
-          //   children: [],
-          // )
-
-
-         ],
-       ),
+          //MOVIES TEXT
+          Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.fromLTRB(35, 0, 0, 10),
+              child: Text('Movies',
+                  textAlign: TextAlign.left,
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.bold, fontSize: 32))),
+          Expanded(
+            child: GridView.count(
+            crossAxisCount: 2,
+            childAspectRatio: ((MediaQuery.of(context).size.width/1.8)/(MediaQuery.of(context).size.height/2.8)),
+            children: List.generate(listMovies.length, (index){
+                return  MovieCard(movie: listMovies[index]);
+                
+              }
+            ),
+          )
+          ),
+          
+        ],
+      ),
     );
   }
 }
-
-
--- lib/src/home/widgets/movie_card.dart
+++ lib/src/home/widgets/movie_card.dart
@@ -1,49 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:moviesapp/src/home/models/movie.dart';
+import 'package:moviesapp/src/movie_details/moviepage.dart';
 
 class MovieCard extends StatelessWidget {
-  const MovieCard({ Key? key, required this.imageMovie, required this.nameMovie}) : super(key: key);
+  const MovieCard({Key? key, required this.movie}) : super(key: key);
 
-  final String imageMovie;
-  final String nameMovie;
+  final Movie movie;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
-      width: MediaQuery.of(context).size.width/2,
-      padding: const EdgeInsets.fromLTRB(30, 15.0, 30, 15.0),
-      alignment: Alignment.center,
-      child: Column(
-        // mainAxisAlignment: MainAxisAlignment.start,
-        // crossAxisAlignment: CrossAxisAlignment.start, 
-        children: [
-        //IMAGE
-          Container(
-            height: 200,
-            width: 150,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              image: DecorationImage(
-                image: NetworkImage(imageMovie),
-                fit: BoxFit.fill
-              )
+        // color: Colors.red,
+        width: MediaQuery.of(context).size.width / 1.8,
+        height: MediaQuery.of(context).size.height / 1.5,
+        padding: const EdgeInsets.fromLTRB(30, 15.0, 30, 15.0),
+        alignment: Alignment.center,
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //IMAGE
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MoviePage(movie: movie)));
+              },
+              child: Container(
+                height: 200,
+                width: 150,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                        image: NetworkImage(movie.image.toString()),
+                        fit: BoxFit.fill)),
+              ),
             ),
-          ),
-          //MOVIE NAME
-          Container(
-            padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-            child: Text(
-                nameMovie,
-                // textAlign: TextAlign.center,
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 13
-                )
-            )
-          ),
-        ],
-      )
-    );
+
+            //MOVIE NAME
+            Container(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                child: Text(
+                    movie.name.toString().length > 17
+                        ? movie.name.toString().substring(0, 13) + "..."
+                        : movie.name.toString(),
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w500, fontSize: 13))),
+          ],
+        ));
   }
 }
